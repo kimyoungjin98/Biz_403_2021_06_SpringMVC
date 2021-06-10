02e8098f901ed1ebc4c711241e8d0a9e4fd197fb
kimyoungjin98 <gyu250@naver.com>
2021-06-10 15:38:20 +0900

    login 까지

--- SpMVC_020_JDBC/src/main/java/com/callor/jdbc/controller/HomeController.java
+++ SpMVC_020_JDBC/src/main/java/com/callor/jdbc/controller/HomeController.java
@@ -18,43 +18,37 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class HomeController {
-	
+
 	// string.properties 파일에 설정된 속성값 가져와서 변수에 setting
 	@Value("${user.name}")
 	protected String user_name;
-	
+
 	@Value("${user.email}")
 	protected String user_email;
 
 	/*
 	 * 보편적인 Spring에서 bean을 사용하는 코드
-	 * @Autowired
-	 * private BookDao bookdao
-	 * 이 코드에서 메모리 leak(누수)현상이 보고되어
-	 * 다음의 코드를 권장한다
+	 * 
+	 * @Autowired private BookDao bookdao 이 코드에서 메모리 leak(누수)현상이 보고되어 다음의 코드를 권장한다
 	 */
-	
+
 	// protected final BookDao bookDao;
 	protected final RentService rentSerivce;
-	
+
 	public HomeController(RentService rentSerivce) {
-		
+
 		this.rentSerivce = rentSerivce;
-		
-		
+
 	}
-	
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		
+
 		log.debug("User Name : {}", user_name);
 		log.debug("User Email : {}", user_email);
 		rentSerivce.viewBookAndComp();
-		
-			
+
 		return "home";
 	}
-	
-}
 
+}
